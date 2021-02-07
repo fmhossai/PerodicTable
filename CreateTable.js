056e5ce5cfe2f2bb439be8d02237edec4a6418e5
Faisal <faisal.m.hossain@gmail.com>
2021-02-07 16:11:23 -0700

    Refactoring code started

--- CreateTable.js
+++ CreateTable.js
@@ -1,91 +1,146 @@
 //create td's for table
-export function Create(cls, idd){
-    var ccc = document.querySelectorAll(`#${idd}~tr`);
-    for (var x of ccc){
-      var h = document.createElement("td");
-      h.setAttribute("class",`${cls} groups`);
-      x.insertAdjacentElement("beforeend",h);
+export function Create(className, classID) {
+  var elements = document.querySelectorAll(`#${classID}~tr`);
+  for (var element of elements) {
+    var elementNode = document.createElement("td");
+    elementNode.setAttribute("class", `${className} groups`);
+    element.insertAdjacentElement("beforeend", elementNode);
+  }
+}
+
+//inserting perodic table data elements here
+export function insertData(data, Group, ClassName) {
+  let x = 0;
+  for (let element in data) {
+    let hello = document.querySelectorAll(ClassName);
+    if (data[element]["number"] == Group[x]) {
+      hello[x].setAttribute("id", `${data[element]["symbol"].toLowerCase()}`);
+      let mass = parseFloat(data[element]["atomic_mass"]);
+      let number = data[element]["number"];
+      hello[x].textContent = number + "\n" + symbol + "\n" + mass.toFixed(2);
+      x++;
     }
   }
+}
+export function createTable(data){
+  let currentX = 0;
+  let currentY = 0;
+  let prevX = 0;
+  let tableNode = document.getElementById("table_body")
+  let tableNode_2 = document.getElementById("table_body_2")
+  let currentRowElementNode;
+  data.forEach(element =>{
+    // create Element spaces
+    if(((element.number >= 57) && (element.number <= 71))|| ((element.number >= 89) && (element.number <=103))){
+      let rowElementNode = document.getElementById(`row${element.ypos}`)
+      if(!rowElementNode){
+        let createRowElementNode = document.createElement('tr');
+        createRowElementNode.setAttribute('id', `row${element.ypos}`)
+        tableNode_2.insertAdjacentElement('beforeend', createRowElementNode)
+        rowElementNode = createRowElementNode;
+      }
+      const newElementNode = createElementNode(element);
+      rowElementNode.insertAdjacentElement('beforeend', newElementNode);
+      return;
 
-  //inserting perodic table data elements here
-  export function insertData(data,Group,ClassName){
-    let x = 0;
-    for (var i in data){
-        let hello = document.querySelectorAll(ClassName);
-        if (data[i]['number'] == Group[x]){
-          hello[x].setAttribute("id",`${data[i]['symbol'].toLowerCase()}`)
-          var mass = parseFloat(data[i]['atomic_mass'])
-          var number = data[i]['number']
-          var symbol = data[i]['symbol']
-          hello[x].textContent = number+'\n'+symbol+'\n'+ mass.toFixed(2);
-          x++;  
-        }   
     }
+    currentX = element.xpos
+    // if currentRow different from element row, create a new row
+    if(currentY < element.ypos){
+      let rowElementNode = document.createElement('tr');
+      tableNode.insertAdjacentElement("beforeend", rowElementNode)
+      currentRowElementNode = rowElementNode;
+      currentY = element.ypos
     }
+    // create blank spaces between elements
+    if((currentX - prevX) > 1){
+      for(let i= 1; i<(currentX - prevX); i++){
+        let blankElementNode = document.createElement('td');
+        blankElementNode.setAttribute('class', `Column${i + 1}`)
+        currentRowElementNode.insertAdjacentElement('beforeend', blankElementNode)
+      }
+    }
+    const newElementNode = createElementNode(element)
+    currentRowElementNode.insertAdjacentElement('beforeend', newElementNode);
+    prevX = element.xpos;
+    
+  })
+}
+function createElementNode(element){
+    const {symbol, number, atomic_mass} = element
+    const elementNode = document.createElement("td");
+    elementNode.setAttribute('class', `Column${element.xpos}`)
+    let mass = parseFloat(atomic_mass).toFixed(2);
+    elementNode.textContent = `${number} \n ${symbol} \n ${mass}`
+    return elementNode
+}
 //creating blank tds
-export function blnk(spaces,row){
-    let g = document.getElementById(row);
-    for (let i=0;i<spaces;i++){
-        var k = document.createElement("td");
-        // k.textContent = 'x'+'\n'+'x'+'\n'+'00.00';
-        k.setAttribute("class","blank");
-        if (row == "third"){
-          g.insertAdjacentElement("beforebegin",k);
-        }
-        else{
-        g.insertAdjacentElement("beforeend",k);
-        }
-    }
+export function blnk(spaces, row) {
+  let rowID = document.getElementById(row);
+  for (let i = 0; i < spaces; i++) {
+    var elementNode = document.createElement("td");
+    elementNode.setAttribute("class", "blank");
+    rowID.insertAdjacentElement("beforeend", elementNode);
+  }
 }
 
 //creating blank tds at the bottom of the periodic table
-export function blnk2(row,period){
-  let g = document.getElementById(row)
-  for (let i=1;i<18;i++){
-    if ((i == 1) || (i ==2)){
-      var c = document.createElement("td")
-      c.setAttribute("class", "blank")
-      g.insertAdjacentElement("beforeend",c)
-    }
-    else{
-      var c = document.createElement("td")
-      c.setAttribute("class",`${period} groups`)
-      g.insertAdjacentElement("beforeend",c)
-    }
+export function blnk2(row, period) {
+  let g = document.getElementById(row);
+  for (let i = 1; i < 16; i++) {
+    var c = document.createElement("td");
+      c.setAttribute("class", `${period} groups`);
+      g.insertAdjacentElement("beforeend", c);
   }
 }
 //function to create the periodic table programmatically
-export function choose(str){
-  switch(str){
-    case "1" : return [1,3,11,19,37,55,87];
-    case "2" : return [4,12,20,38,56,88];
-    case "3" : return [21,39];
-    case "4" : return [22,40,72,104];
-    case "5" : return [23,41,73,105];
-    case "6" : return [24,42,74,106];
-    case "7" : return [25,43,75,107];
-    case "8" : return [26,44,76,108];
-    case "9" : return [27,45,77,109];
-    case "10" : return [28,46,78,110]
-    case "11" : return [29,47,79,111];
-    case "12" : return [30,48,80,112];
-    case "13" : return [5,13,31,49,81,113];
-    case "14" : return [6,14,32,50,82,114];
-    case "15" : return [7,15,33,51,83,115];
-    case "16" : return [8,16,34,52,84,116];
-    case "17" : return [9,17,35,53,85,117];
-    case "18" : return [2,10,18,36,54,86,118];
+export function choose(str) {
+  switch (str) {
+    case "1":
+      return [1, 3, 11, 19, 37, 55, 87];
+    case "2":
+      return [4, 12, 20, 38, 56, 88];
+    case "3":
+      return [21, 39];
+    case "4":
+      return [22, 40, 72, 104];
+    case "5":
+      return [23, 41, 73, 105];
+    case "6":
+      return [24, 42, 74, 106];
+    case "7":
+      return [25, 43, 75, 107];
+    case "8":
+      return [26, 44, 76, 108];
+    case "9":
+      return [27, 45, 77, 109];
+    case "10":
+      return [28, 46, 78, 110];
+    case "11":
+      return [29, 47, 79, 111];
+    case "12":
+      return [30, 48, 80, 112];
+    case "13":
+      return [5, 13, 31, 49, 81, 113];
+    case "14":
+      return [6, 14, 32, 50, 82, 114];
+    case "15":
+      return [7, 15, 33, 51, 83, 115];
+    case "16":
+      return [8, 16, 34, 52, 84, 116];
+    case "17":
+      return [9, 17, 35, 53, 85, 117];
+    case "18":
+      return [2, 10, 18, 36, 54, 86, 118];
   }
 }
-export function showElements(elements,symbols){
-    for(let element of symbols){
-        let ElementNode = document.getElementById(element)
-        if (elements.includes(element)){
-            ElementNode.style.visibility = "visible"
-        }
-        else{
-            ElementNode.style.visibility = "hidden"
-        }
+export function showElements(elements, symbols) {
+  for (let element of symbols) {
+    let ElementNode = document.getElementById(element);
+    if (elements.includes(element)) {
+      ElementNode.style.visibility = "visible";
+    } else {
+      ElementNode.style.visibility = "hidden";
     }
+  }
 }
--- main.js
+++ main.js
@@ -1,77 +1,103 @@
-import {Create, insertData, blnk,blnk2,choose,showElements} from "./CreateTable.js"
-if (document.readyState == 'loading'){
-  document.addEventListener('DOMContentLoaded', ready)
+import {
+  Create,
+  createTable,
+  insertData,
+  blnk,
+  blnk2,
+  choose,
+  showElements,
+} from "./CreateTable.js";
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
 }
-else{
-  ready()
-}
-function ready(){
+function ready() {
   //elements on the bottom
-  const Period1 = [57,58,59,60,61,62,63,64,65,66,67,68,69,70,71]
-  const Period2 = [89,90,91,92,93,94,95,96,97,98,99,100,101,102,103]
+  const Period1 = [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71];
+  const Period2 = [
+    89,
+    90,
+    91,
+    92,
+    93,
+    94,
+    95,
+    96,
+    97,
+    98,
+    99,
+    100,
+    101,
+    102,
+    103,
+  ];
   const inputfield = document.getElementById("searchElement");
   var dict = {};
   var symbols = [];
   //main function which uses the functions
-  function main(data1){
+  function main(data1) {
     //first create all the tr and td elements
-    for (var i=1; i<13;i++){
-      if (i === 1) Create(`Group${i}`,"firstRow")
-      else if (i === 2) Create(`Group${i}`,"secondRow");
-      else Create(`Group${i}`,"restRows")
-    }
+    createTable(data1);
+    // for (var i = 1; i < 13; i++) {
+    //   if (i === 1) Create(`Group${i}`, "firstRow");
+    //   else if (i === 2) Create(`Group${i}`, "secondRow");
+    //   else Create(`Group${i}`, "restRows");
+    // }
     //create the blank tds which are in the middle of the table
-    blnk(16,"secondRow");
-    blnk(10,"thirdRow");
-    blnk(10,"restRows");
+    blnk(16, "secondRow");
+    blnk(10, "thirdRow");
+    blnk(10, "restRows");
     //create the rest of the td elements
-    for(var i=13;i<19;i++){
-      if (i == 18) Create(`Group${i}`,"firstRow")
-      else Create(`Group${i}`,"secondRow")
+    for (var i = 13; i < 19; i++) {
+      if (i == 18) Create(`Group${i}`, "firstRow");
+      else Create(`Group${i}`, "secondRow");
     }
     //insert periodic data in all tds
-    for (var i=1;i<19;i++){
-      insertData(data1,choose(`${i}`),`.Group${i}`)
+    for (var i = 1; i < 19; i++) {
+      insertData(data1, choose(`${i}`), `.Group${i}`);
     }
     //create a object of atomic symbols and names;used for search function
-    for (let element of data1){
-      dict[element.symbol.toLowerCase()] = element.name.toLowerCase()
-      symbols.push(element.symbol.toLowerCase())
+    for (let element of data1) {
+      dict[element.symbol.toLowerCase()] = element.name.toLowerCase();
+      symbols.push(element.symbol.toLowerCase());
     }
-    console.log(symbols)
   }
 
   //search bar function
-  inputfield.addEventListener("keyup",(e)=>{
-    let searchStr = inputfield.value.toLowerCase()
-    searchStr = searchStr.replace(/\s/g,'')
-    console.log(searchStr)
-    if(searchStr.length === 1){
-      showElements(symbols.filter(symbol => symbol === searchStr), symbols)
+  inputfield.addEventListener("keyup", (e) => {
+    let searchStr = inputfield.value.toLowerCase();
+    searchStr = searchStr.replace(/\s/g, "");
+    console.log(searchStr);
+    if (searchStr.length === 1) {
+      showElements(
+        symbols.filter((symbol) => symbol === searchStr),
+        symbols
+      );
+    } else if (searchStr === "") {
+      showElements(symbols, symbols);
+    } else {
+      let filteredElements = symbols.filter((symbol) =>
+        dict[symbol].includes(searchStr)
+      );
+      showElements(filteredElements, symbols);
     }
-    else if(searchStr === ""){
-        showElements(symbols,symbols)
-    }
-    else{
-      let filteredElements = symbols.filter(symbol => dict[symbol].includes(searchStr))
-      showElements(filteredElements, symbols)
-    }
-  })
+  });
 
   //used to fetch data from api and call some other functions to finish creating the periodic table
-  async function fetchData(){
-    const response = await fetch("PeriodicTableJSON.json")
-    const {elements:data} = await response.json()
-    data.pop()
-    console.log(data)
-    main(data)
-    let zzz = document.querySelectorAll(".Group3.groups")
-    zzz[2].textContent = '57-71'
-    zzz[3].textContent = '89-103'
-    blnk2("fou","Period1")
-    blnk2("fif","Period2")
-    insertData(data,Period1,".Period1.groups")
-    insertData(data,Period2,".Period2.groups")
+  async function fetchData() {
+    const response = await fetch("PeriodicTableJSON.json");
+    const { elements: data } = await response.json();
+    data.pop();
+    console.log(data);
+    main(data);
+    let blankSpace = document.querySelectorAll(".Group3.groups");
+    blankSpace[2].textContent = "57-71";
+    blankSpace[3].textContent = "89-103";
+    blnk2("fourthRow", "Period1");
+    blnk2("fifthRow", "Period2");
+    insertData(data, Period1, ".Period1.groups");
+    insertData(data, Period2, ".Period2.groups");
   }
 
   //call the fetch
