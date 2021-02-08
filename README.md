1c5c4dfaf2b7c5a34167b23a08249cee0a507f29
Faisal <faisal.m.hossain@gmail.com>
2021-02-07 18:14:49 -0700

    Cleaned up code and Search function now working

--- README.md
+++ README.md
@@ -8,3 +8,14 @@ Hosted on Netfliy here(https://interactive-periodic-table.netlify.com)
 A search function, which helps you locate a certain element using thier symbol or name.
 Buttons that highlight search group elements, like Alkaline Earth Metals, Alkali Metals, Noble Gases and many more.
 Quickly search back and forth between highlighting and non-highlighting elements, so you would know what a basic Perodic Table would look like without hightlighting, useful for studying.
+
+# How to setup
+
+1. Clone this Git repo
+
+2. change into project director
+
+3. type npm start to host a local web server
+
+
+
--- main.js
+++ main.js
@@ -1,105 +1,81 @@
 import {
-  Create,
   createTable,
-  insertData,
-  blnk,
-  blnk2,
-  choose,
-  showElements,
 } from "./CreateTable.js";
 if (document.readyState == "loading") {
   document.addEventListener("DOMContentLoaded", ready);
 } else {
   ready();
 }
-function ready() {
-  //elements on the bottom
-  const Period1 = [57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71];
-  const Period2 = [
-    89,
-    90,
-    91,
-    92,
-    93,
-    94,
-    95,
-    96,
-    97,
-    98,
-    99,
-    100,
-    101,
-    102,
-    103,
-  ];
+async function ready() {
   const inputfield = document.getElementById("searchElement");
-  var dict = {};
-  var symbols = [];
+  let elements = [];
+  // array of objects with the following properties
+  /*
+  {
+    symbol: symbol of element
+    name: name of element
+    function visible(): shows the element on the screen or not.
+  }
+  */
   //main function which uses the functions
-  function main(data1) {
-    //first create all the tr and td elements
-    createTable(data1);
-    // for (var i = 1; i < 13; i++) {
-    //   if (i === 1) Create(`Group${i}`, "firstRow");
-    //   else if (i === 2) Create(`Group${i}`, "secondRow");
-    //   else Create(`Group${i}`, "restRows");
-    // }
-    //create the blank tds which are in the middle of the table
-    blnk(16, "secondRow");
-    blnk(10, "thirdRow");
-    blnk(10, "restRows");
-    //create the rest of the td elements
-    for (var i = 13; i < 19; i++) {
-      if (i == 18) Create(`Group${i}`, "firstRow");
-      else Create(`Group${i}`, "secondRow");
-    }
-    //insert periodic data in all tds
-    for (var i = 1; i < 19; i++) {
-      insertData(data1, choose(`${i}`), `.Group${i}`);
-    }
-    //create a object of atomic symbols and names;used for search function
-    for (let element of data1) {
-      dict[element.symbol.toLowerCase()] = element.name.toLowerCase();
-      symbols.push(element.symbol.toLowerCase());
-    }
+  function main(data) {
+
+    createTable(data);
+    const elementsData = data.map(element => ({
+      symbol: element.symbol.toLowerCase(),
+      name: element.name.toLowerCase(),
+      visible: function(bool){
+        let elementNode = document.getElementById(`${this.symbol}`)
+        if(bool){
+          elementNode.style.visibility = "visible"
+        }
+        else{
+          elementNode.style.visibility = "hidden"
+        }
+      }
+    }))
+    elements = elementsData
   }
 
   //search bar function
   inputfield.addEventListener("keyup", (e) => {
-    let searchStr = inputfield.value.toLowerCase();
-    searchStr = searchStr.replace(/\s/g, "");
+    let searchStr = inputfield.value.toLowerCase().trim();
+    // searchStr = searchStr.replace(/\s/g, "");
     console.log(searchStr);
-    if (searchStr.length === 1) {
-      showElements(
-        symbols.filter((symbol) => symbol === searchStr),
-        symbols
-      );
+    if (searchStr.length === (1 || 2)) {
+      elements.forEach((element)=>{
+        if(searchStr === element.symbol){
+          element.visible(true)
+        }
+        else{
+          element.visible(false)
+        }
+      })
+
     } else if (searchStr === "") {
-      showElements(symbols, symbols);
-    } else {
-      let filteredElements = symbols.filter((symbol) =>
-        dict[symbol].includes(searchStr)
-      );
-      showElements(filteredElements, symbols);
+      elements.forEach(element => element.visible(true))
+    } 
+    else {
+      elements.forEach((element)=>{
+        if(element.name.includes(searchStr)){
+          element.visible(true)
+        }
+        else{
+          element.visible(false)
+        }
+      })
     }
   });
 
   //used to fetch data from api and call some other functions to finish creating the periodic table
-  async function fetchData() {
-    const response = await fetch("PeriodicTableJSON.json");
+  async function fetchData(url) {
+    const response = await fetch(url);
     const { elements: data } = await response.json();
     data.pop();
-    console.log(data);
-    main(data);
-    let blankSpace = document.querySelectorAll(".Group3.groups");
-    blankSpace[2].textContent = "57-71";
-    blankSpace[3].textContent = "89-103";
-    blnk2("fourthRow", "Period1");
-    blnk2("fifthRow", "Period2");
-    insertData(data, Period1, ".Period1.groups");
-    insertData(data, Period2, ".Period2.groups");
+    return data
   }
 
   //call the fetch
-  fetchData();
+  const data = await fetchData("PeriodicTableJSON.json");
+  main(data);
 }
